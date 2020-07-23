0e99921c7d1293313b0be1b35627974d5ca0f8ac
Cendeal <5205093You>
2020-07-23 11:47:16 +0800

    [FizzBuzz][Cendeal]test:add should_return_1_when_play_fizz_buzz_given_1 test

---
+++ src/test/java/junit5/tdd/FizzBuzzUnitTest.java
@@ -0,0 +1,20 @@
+package junit5.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FizzBuzzUnitTest {
+    @Test
+    public void should_return_1_when_play_fizz_buzz_given_1(){
+        //given
+        int given = 1;
+        //when
+        FizzBuzz game = new FizzBuzz();
+        String actual = game.play(given);
+        //then
+        String expect = "1";
+        assertEquals(expect,actual);
+    }
+}
